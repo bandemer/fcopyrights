f14851bd86721160f04440c3f09b38174721f0bb
Matthias Bandemer <git@bandemer.net>
2016-02-18 23:09:01 +0100

    Bug fixes in contentScriptFile.js

--- data/contentScriptFile.js
+++ data/contentScriptFile.js
@@ -1,19 +1,17 @@
 /**
- * ContentScriptFile for adding download to fota details pages
+ * ContentScriptFile for adding download to fotolia details pages
  */
-window.addEventListener('click', function(event) {
-    if (event.target.id == 'fcopyrightsdownload') {
-        self.port.emit('getCopyrightFile', document.body.innerHTML);
-    } else {
-        return true;
-    }
-}, false);
-
 const fcreditsPattern = /([a-z]+: #[0-9]+ \| [a-z]+: <a[^>]* href="\/p\/[0-9]+">[^<]+<\/a>)/i;
-var html = document.body.innerHTML;
-if (html.search(fcreditsPattern) != -1) {
-    html = html.replace(fcreditsPattern, "$1 <a href=\"#\" id=\"fcopyrightsdownload\"><span class=\"sp-16 ico-download\"></span> Download Copyrights</a>");
-    document.body.innerHTML = html;
+var elements = document.getElementsByClassName('content-preview');
+if (elements.length > 0) {
+    var html = elements[0].innerHTML;
+    if (html.search(fcreditsPattern) != -1) {
+        html = html.replace(fcreditsPattern, "$1 <a id=\"fcopyrightsdownload\"> <span class=\"sp-16 ico-download\"></span>Download Copyrights</a>");
+        elements[0].innerHTML = html;
+    }
 }
 
-
+document.getElementById('fcopyrightsdownload').addEventListener('click', function(event) {
+    self.port.emit('getCopyrightFile', document.body.innerHTML);
+    return false;
+}, false);
